18380a2b5fd8c998ee32fcb40822332b224711ce
mallollaa <mah41411@gmail.com>
2023-02-01 18:09:20 +0300

    done

--- airport/urls.py
+++ airport/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from flights.views import ListAPIView 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("flights_list/",ListAPIView.as_view() , name="flights-list"),
+    path("bookings_list/",ListAPIView.as_view(), name="bookings-list "),
 ]
--- flights/views.py
+++ flights/views.py
@@ -1,1 +1,15 @@
 from .models import Booking, Flight
+from rest_framework.generics import ListAPIView 
+from flights.models import Flight
+from .serializers import FlightListSerializer , BookingsListSerializer
+
+#the serializer is doing here is taking each individual queryset object provided 
+# and converting it into JSON format
+
+class ListView(ListAPIView):
+    queryset = Flight.objects.all() # read from the database
+    serializer_class = FlightListSerializer
+
+class BookingsListView(ListAPIView):
+    queryset = Booking.objects.all() 
+    serializer_class = BookingsListSerializer
